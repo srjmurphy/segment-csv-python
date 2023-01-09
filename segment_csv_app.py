97b0c5673f833d306f8dac44ed0e686d35206718
Stephen Murphy <smurphyrj@gmail.com>
2023-01-09 19:11:36 +0000

    updated py code w/ error handling

--- segment_csv_app.py
+++ segment_csv_app.py
@@ -1,9 +1,10 @@
 import requests
 import pandas as pd
 import base64
-from flask import Flask, request, render_template, make_response, redirect
+from flask import Flask, request, render_template, make_response, redirect, flash
 
 app = Flask(__name__)
+app.secret_key = 'your-secret-key'
 
 @app.route('/')
 def index():
@@ -11,34 +12,41 @@ def index():
 
 @app.route('/send_messages', methods=['POST'])
 def send_messages():
-    # Retrieve the uploaded file and write key from the request object
-    csv_file = request.files['csv_file']
-    write_key = request.form['write_key']
+    try:
+        # Retrieve the uploaded file and write key from the request object
+        csv_file = request.files['csv_file']
+        write_key = request.form['write_key']
 
-    # Base64-encode the write key
-    write_key_b64 = base64.b64encode(write_key.encode('utf-8')).decode('utf-8')
+        # Base64-encode the write key
+        write_key_b64 = base64.b64encode(write_key.encode('utf-8')).decode('utf-8')
 
-    # Read in the CSV file using Pandas
-    df = pd.read_csv(csv_file)
+        # Read in the CSV file using Pandas
+        df = pd.read_csv(csv_file)
 
-    # Iterate over the rows of the CSV file and send a message for each row using the Segment API
-    for index, row in df.iterrows():
-        message = row['message']
-        user_id = row['user_id']
+        # Iterate over the rows of the CSV file and send a message for each row using the Segment API
+        for index, row in df.iterrows():
+            message = row['message']
+            user_id = row['user_id']
 
-        payload = {
-            'userId': user_id,
-            'traits': {
-                'message': message
+            payload = {
+                'userId': user_id,
+                'traits': {
+                    'message': message
+                }
             }
-        }
-        requests.post('https://api.segment.io/v1/identify', json=payload, headers={
-            'Authorization': 'Basic ' + write_key_b64,
-            'Content-Type': 'application/json'
-        })
+            requests.post('https://api.segment.io/v1/identify', json=payload, headers={
+                'Authorization': 'Basic ' + write_key_b64,
+                'Content-Type': 'application/json'
+            })
 
-    # Redirect the user's browser to the index page
-    return redirect('/')
+        # Redirect the user's browser to the index page and display a success message
+        flash('Messages successfully sent to Segment!')
+        return redirect('/')
+
+    except Exception as e:
+        # If an error occurs, display an alert to the user
+        flash('An error occurred while sending messages to Segment: {}'.format(e))
+        return redirect('/')
 
 if __name__ == '__main__':
     app.run()
